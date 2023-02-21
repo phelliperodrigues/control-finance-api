e1b9d02b7eb317bec11b390ae682c3c61ca6c5ee
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2023-02-21 17:49:46 -0300

    :tada: fisrt commit

---
+++ src/main/java/dev/phelliperodrigues/controlfinance/ControlFinanceApplication.java
@@ -0,0 +1,13 @@
+package dev.phelliperodrigues.controlfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlFinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/phelliperodrigues/controlfinance/ControlFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.phelliperodrigues.controlfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
